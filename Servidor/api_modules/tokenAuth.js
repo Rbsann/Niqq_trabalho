53ad6dc1f8f4aad74a7adf00bf3523df587d1324
Bruno Miyamoto <bmiyamoto@gmail.com>
2017-08-02 18:12:03 -0300

    Servidor: desabilitada renovação e invalidação de tokens

--- Servidor/api_modules/tokenAuth.js
+++ Servidor/api_modules/tokenAuth.js
@@ -21,33 +21,34 @@ module.exports = function (req, res, next) {
 			user = foundUser;
 			return user.isValidToken(token);
 		})
-		.then(result => {
-			var tokenAge = Date.now() / 1000 - tokenPayload.iat; // seconds since token was issued
-			if (tokenAge > tokenExpirationAge) { // token has expired
-				return user.revokeToken(token)
-					.then(result => {
-						throw Error("TOKEN_EXPIRED");
-					});
-			} else if (tokenAge > tokenRenewalAge) { // token is due for renewal
-				return user.newToken(req, tokenPayload.dynasty) // issue new token, but preserve dynasty
-					.then(token => {
-						res.locals.newToken = token; // set res variable
-					})
-					.then(() => user.revokeToken(token)) // revoke old token
-					.catch(error => {
-						console.log(error);
-						throw error;
-					});
-			}
-		})
+		//TODO: verify consistency of token renovation and revoking
+		// .then(result => {
+		// 	var tokenAge = Date.now() / 1000 - tokenPayload.iat; // seconds since token was issued
+		// 	if (tokenAge > tokenExpirationAge) { // token has expired
+		// 		return user.revokeToken(token)
+		// 			.then(result => {
+		// 				throw Error("TOKEN_EXPIRED");
+		// 			});
+		// 	} else if (tokenAge > tokenRenewalAge) { // token is due for renewal
+		// 		return user.newToken(req, tokenPayload.dynasty) // issue new token, but preserve dynasty
+		// 			.then(token => {
+		// 				res.locals.newToken = token; // set res variable
+		// 			})
+		// 			.then(() => user.revokeToken(token)) // revoke old token
+		// 			.catch(error => {
+		// 				console.log(error);
+		// 				throw error;
+		// 			});
+		// 	}
+		// })
 		.then(() => {
 			res.locals.user = user;
 			next();
 		})
 		.catch(error => {
 			if (error.message === "INVALID_TOKEN") { // if token was authenticated but is revoked, revoke dynasty
-				user.killTokenDynasty(tokenPayload.dynasty)
-					.catch(error => console.log(error));
+				// user.killTokenDynasty(tokenPayload.dynasty)
+				// 	.catch(error => console.log(error));
 			}
 			sendResponseUnauthorized(res);
 		});
