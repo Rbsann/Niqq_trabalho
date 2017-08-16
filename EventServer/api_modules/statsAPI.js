67ad7f0dc3a1b316ff5dff208b75a306400a6bf4
raul <raulmanzas@gmail.com>
2017-08-16 20:14:35 -0300

    Refatoração e implementação do histograma

--- EventServer/api_modules/statsAPI.js
+++ EventServer/api_modules/statsAPI.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Stats = require('../models/stats.js');
+
 /*
     Autenticação nesta API é opcional!
 */
@@ -14,42 +15,8 @@ router.get('/test', (req, res) => {
     res.send('ok');
 });
 
-// Quando a api receber uma requisição de verbo POST, 
-// router.get('/', function (req, res, next){ //cria pagina para visualizar stats
-//     res.render('stats');
-// });
-
-//router.get('/botSignup', function(req, res, next){ //clicavel, on-click mostra stats do fill up do bot
- //   var contagem = Stats.botSignUpPage();
- //   var data = [
-  //      {
-   //         x: ["Ended", "Downloaded", "Not Downloaded"],
-    //        y: contagem,
-   //         type: "bar"
-  //      }
-  //  ];
-   // res.json(data);
-   // var graphOptions = { filename: "basic-bar", fileopt: "overwrite" };
-  //  plotly.plot(data, graphOptions, function (err, msg) {
-        //console.log(msg);
-    //});
-//});
-
-//router.get('/fracSignup', function(req, res, next){// stats do form
-  //  var contagem = Stats.formSignUp();
-   // var data = [
-     //   {
-       //     x: ["pView", "step1", "step2", "down", "notDown"],
-         //   y: contagem,
-          //  type: "bar"
-       // }
-   // ];
-   // res.json(data);
-//});
-
-
-router.get('/fracSignup', function(req, res, next){// stats do frac sign up
-    Stats.fracSignup().then((contagem) => {
+router.get('/fractionedSignup', function(req, res, next){// stats do frac sign up
+    Stats.fractionedSignup().then((contagem) => {
         var chart = {
             type: 'bar',
             data:{
