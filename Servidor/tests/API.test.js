2df98b73d7c82e54c220be6151150e099978efea
raul <raulmanzas@gmail.com>
2017-08-19 22:34:49 -0300

    Início da implementação dos testes de integração na userAPI

--- Servidor/tests/API.test.js
+++ Servidor/tests/API.test.js
@@ -29,6 +29,9 @@ describe("API integration test", function() {
 		},
 		encryptedData: {}
 	};
+
+	var tempToken = "";
+
 	describe("AccountAPI", function() {
 		describe("Test set up", function() {
 			it ("should delete previous test user", function(done) {
@@ -63,7 +66,7 @@ describe("API integration test", function() {
 		});
 
 		describe("Login/logout test", function() {
-			var tempToken = "";
+			// var tempToken = "";
 			it("should perform login", function(done) {
 				request(app).post("/account/login").send({email: user.email, password: user.password})
 					.end(function(err, res) {
@@ -86,5 +89,144 @@ describe("API integration test", function() {
 		});
 
 	});
-	
+
+	describe("UserAPI", function(){
+		var loggedInUser = {
+			'token': tempToken,
+			'email': user.email
+		};
+
+		before("User login before tests",function(done){
+			this.timeout(5000);
+			request(app).post("/account/login").send({email: user.email, password: user.password})
+			.end(function(err, res) {
+				var data = processAPIResponse(res);
+				expect(data.firstName).to.be.equal(user.data.name.first);
+				expect(data.gender).to.be.equal(user.data.gender);
+				expect(data.token).to.be.a("string");
+				tempToken = data.token;
+				done();
+			});
+		});
+
+		describe("Get all user information test", function(){
+			it("should not return data to unauthorized users", function(done){
+				request(app).post("/user/getAllInfo").send({token: '', email: ''})
+				.end(function(err, res){
+					expect(res.statusCode).to.be.equal(401);
+					done();
+				});
+			});
+
+			it("should query all user information", function(done){
+				request(app).post("/user/getAllInfo").send({
+					token: tempToken,
+					email: user.email
+				})
+				.end(function(err, res){
+					let data = processAPIResponse(res);
+					expect(data.data.name.first).to.be.equal(user.data.name.first);
+					expect(data.encryptedData).to.not.be.null;
+					done();
+				});
+			});
+
+		});
+
+		describe("User password tests", function(){
+			it("should not change password if original password is wrong", function(done){
+				request(app).post('/user/changePassword').send({ 
+					token: tempToken,
+					email: user.email,
+					currentPassword: "wrongPassword",
+					newPassword: '123',
+					encryptedData: user.encryptedData,
+				})
+				.end(function(err, res){
+					expect(res.body.error).to.be.equal(1);
+					done();
+				});
+			});
+
+			it("should change user password" , function(done){
+				request(app).post('/user/changePassword').send({ 
+					token: tempToken,
+					email: user.email,
+					currentPassword: user.password,
+					newPassword: user.password,
+					encryptedData: user.encryptedData,
+				})
+				.end(function(err, res){
+					let data = processAPIResponse(res);
+					expect(data.changed).to.be.true;
+					done();
+				});
+			});
+
+			//TODO: Verificar se alguma correção é necessária no serviço de resetar password
+			// it("should not request password reset with invalid email", function(done){
+			// 	request(app).post('/user/requestPasswordReset').send({ email: user.email})
+			// 	.end(function(err, res){
+			// 		
+			// 		let data = processAPIResponse(res);
+			// 		done();
+			// 	});
+			// });
+
+			// it("should request password reset email", function(done){
+			// 	request(app).post('/user/requestPasswordReset').send({ email: user.email})
+			// 	.end(function(err, res){
+			// 		//TODO: Verificar se correção é necessária no serviço de resetar password
+			// 		let data = processAPIResponse(res);
+			// 		expect(data.requestReceived).to.be.true;
+			// 		done();
+			// 	});
+			// });
+
+			it("should not reset password when password reset code is wrong", function(done){
+				request(app).post('/user/resetPassword').send({ 
+					password: user.password,
+					passwordResetCode: 'wrongCode'
+				})
+				.end(function(err, res){
+					expect(res.body.error).to.not.be.equal(0);
+					done();
+				});
+			});
+			// TODO: Como testar o serviço de resetar senha tem que abrir o email com o código?
+
+		});
+
+		describe("Update user information test", function(){
+			it("should not update user info if user is not logged in", function(done){
+				request(app).post('/user/editInfo').send({
+					email: user.email,
+					token: "invalidToken",
+					data: user.data,
+					encryptedData: user.encryptedData
+				})
+				.end(function(err, res){
+					expect(res.statusCode).to.be.equal(401);
+					done();
+				});
+			});
+
+			it("should update user info", function(done){
+				request(app).post('/user/editInfo').send({
+					email: user.email,
+					token: tempToken,
+					data: user.data,
+					encryptedData: {}
+				})
+				.end(function(err, res){
+					let data = processAPIResponse(res);
+					expect(data.edited).to.be.equal('true');
+					done();
+				});
+			});
+		});
+
+		// describe("Facebook user authentication test", function(){});
+		// describe("User credential tests", function(){});
+	});
 });
