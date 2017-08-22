6a779f9dbaa70c2325845ee1471f0a3fe74a7b44
Thiago Yukio Alves <thiago@LPA015.local>
2017-08-22 18:21:01 -0300

    pageAPI: adicionados elses no tratamento de formato

--- FormClassifierServer/api_modules/pageAPI.js
+++ FormClassifierServer/api_modules/pageAPI.js
@@ -13,19 +13,20 @@ pageRoutes.post("/new", (request, response) => {
   var urls = request.body.urls;
   if (urls === undefined || urls === null) 
     response.status(400).send("Malformed request");
-    
-  var insertionPromises = urls.map(url => {  // array of Promises
-    Page.insert(url); 
-  });
-
-  Promise.all(insertionPromises)
-    .then(_ => {
-      response.send({urlsInserted: true});
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(500).send({urlsInserted: false});
+  else {
+    var insertionPromises = urls.map(url => {  // array of Promises
+      Page.insert(url); 
     });
+
+    Promise.all(insertionPromises)
+      .then(_ => {
+        response.send({urlsInserted: true});
+      })
+      .catch(error => {
+        console.log(error);
+        response.status(500).send({urlsInserted: false});
+      });
+  }
 });
 
 /// Get a random unclassified url to classify
@@ -51,15 +52,16 @@ pageRoutes.post("/classify", (request, response) => {
   var isForm = request.body.isForm;
   if (url === undefined || url === null || isForm === undefined || isForm === null) 
     response.status(400).send("Malformed request");
-
-  Page.classify(payload.url, payload.isForm)
-    .then(_ => {
-      response.send({urlClassified: true});
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(500).send({urlClassified: false});
-    });
+  else {
+    Page.classify(payload.url, payload.isForm)
+      .then(_ => {
+        response.send({urlClassified: true});
+      })
+      .catch(error => {
+        console.log(error);
+        response.status(500).send({urlClassified: false});
+      });
+  }
 });
 
 /// Get a random unclassified url without screenshot to screenshot
@@ -85,15 +87,16 @@ pageRoutes.post("/screenshot", (request, response) => {
   var imageUrl = request.body.imageUrl;
   if (url === undefined || url === null || imageUrl === undefined || imageUrl === null) 
     response.status(400).send("Malformed request");
-
-  Page.updateImageUrl(url, imageUrl)
-    .then(_ => {
-      response.send({imageSaved: true});
-    })
-    .catch(error => {
-      console.log(error);
-      response.status(500).send({imageSaved: false});
-    });
+  else {
+    Page.updateImageUrl(url, imageUrl)
+      .then(_ => {
+        response.send({imageSaved: true});
+      })
+      .catch(error => {
+        console.log(error);
+        response.status(500).send({imageSaved: false});
+      });
+  }
 });
 
 module.exports = pageRoutes;
