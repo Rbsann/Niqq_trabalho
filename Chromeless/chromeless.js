91fef2e2dd9a58791b245fa72a068c93f7da9c99
Thiago Yukio Alves <thiago@LPA015.local>
2017-08-28 18:26:24 -0300

    Chromeless: adicionada função de crawl nos resultados do google

--- Chromeless/chromeless.js
+++ Chromeless/chromeless.js
@@ -1,15 +1,18 @@
 const Chromeless = require('chromeless').default;
 
-///
-/// Entra no site especificado, pega html e retorna uma Promise que resolve o Html
-///
-async function getHtml(url) {
-  const chromeless = new Chromeless({
+
+var remoteOptions = {
     remote: {
       endpointUrl: 'https://qvyi8j05ma.execute-api.us-east-2.amazonaws.com/dev/',
       apiKey: '6b77VIfmIi9UZupDLZBM79YVBuGe7muxwHS2NRO8'
     },
-  });
+  };
+
+///
+/// Entra no site especificado, pega html e retorna uma Promise que resolve o Html
+///
+async function getHtml(url) {
+  const chromeless = new Chromeless(remoteOptions);
 
   chromeless.queue.chrome.options.viewport = {width: 1440, height: 2000, scale: 1};
   chromeless.queue.chrome.options.launchChrome = false;
@@ -31,12 +34,7 @@ async function getHtml(url) {
 /// Entra no site especificado, pega screenshot e retorna uma Promise que resolve o link do S3 do screnshot
 ///
 async function getScreenshot(url) {
-  const chromeless = new Chromeless({
-    remote: {
-      endpointUrl: 'https://qvyi8j05ma.execute-api.us-east-2.amazonaws.com/dev/',
-      apiKey: '6b77VIfmIi9UZupDLZBM79YVBuGe7muxwHS2NRO8'
-    },
-  });
+  const chromeless = new Chromeless(remoteOptions);
 
   chromeless.queue.chrome.options.viewport = {width: 1440, height: 2000, scale: 1};
   chromeless.queue.chrome.options.launchChrome = false;
@@ -56,12 +54,73 @@ async function getScreenshot(url) {
 }
 
 
+///
+/// Chromeless LOCAL entra no Google e retorna json de resultados equivalente ao numero de paginas especificado
+///
+async function googleQuery(url, numberOfPages) {
+  const chromeless = new Chromeless();
+  console.log("Going to google");
+
+  await chromeless
+    .goto(url);
+
+  var results = [];
+  // while (numberOfPages > 0) {
+    console.log("Page #"+1);
+
+    var partialResults = await chromeless
+      .wait('#resultStats')
+      .evaluate(() => {
+        // this will be executed in headless chrome
+        const links = [].map.call(
+          document.querySelectorAll('.g h3 a'),
+          a => (a.href.split("https://www.google.com.br/url?q=")[1].split("&sa=")[0])
+        );
+        // console.log(links);
+        return links;
+      });
+
+      await chromeless.click("#pnnext");
+      console.log("clicked next page");
+      results.concat(partialResults);
+      numberOfPages--;
+
+      var partialResults2 = await chromeless
+      .wait('#resultStats')
+      .evaluate(() => {
+        // this will be executed in headless chrome
+        const links = [].map.call(
+          document.querySelectorAll('.g h3 a'),
+          a => (a.href.split("https://www.google.com.br/url?q=")[1].split("&sa=")[0])
+        );
+        // console.log(links);
+        return links;
+      });
+
+      await chromeless.click("#pnnext");
+      console.log("clicked next page");
+      results.concat(partialResults2);
+      numberOfPages--;
+  // }
+
+  await chromeless.end();
+  return new Promise((resolve, reject) => {
+    resolve(results);
+  });
+}
+
+
+
 module.exports.getHtml = function(url){
-  return Promise.race([getHtml(url),timeout(8000)]);
+  return Promise.race([getHtml(url),timeout(15000)]);
 };
 
 module.exports.getScreenshot = function(url){
   return Promise.race([getScreenshot(url),timeout(15000)]);
+};
+
+module.exports.googleQuery = function(query){
+  return Promise.race([googleQuery(query),timeout(20000)]);
 };
 
 var timeout = function(time) {
--- Chromeless/populate.js
+++ Chromeless/populate.js
@@ -1,9 +1,10 @@
 const chromeless = require("./chromeless.js");
+const gzip = require('./gzip.js');
 var request = require('request');
 var request = require('request-promise-native');
 
 
-var populateTinderScreenshots = function() {
+var populateNinderScreenshots = function() {
   var url;
   var requestOptions = {
     url: 'https://tinder.niqq.co/screenshot',
@@ -26,7 +27,7 @@ var populateTinderScreenshots = function() {
     })
     .then(response => {
       console.log(response);
-      populateTinderScreenshots();
+      populateNinderScreenshots();
     })
     .catch(error => {
       console.log(error); // Print the error if one occurred
@@ -35,10 +36,10 @@ var populateTinderScreenshots = function() {
 };
 
 
-var populateTinderHtml = function() {
+var populateNinderHtml = function() {
   var url;
   var requestOptions = {
-    url: 'http://localhost:8222/html',
+    url: 'https://tinder.niqq.co/html',
     json: true,
     followAllRedirects: true, 
     followOriginalHttpMethod: true,
@@ -53,24 +54,69 @@ var populateTinderHtml = function() {
       return chromeless.getHtml(url);
     })
     .then(html => {
-      requestOptions.body = {url: url, html: html};
+      var body = "<html>"+html.substring(html.indexOf("<body"));  // removes <head>
+      return gzip.zip(body);
+    })
+    .then(zippedHtml => {
+      requestOptions.body = {url: url, html: zippedHtml};
+      // console.log(requestOptions);
       return request.post(requestOptions);
     })
     .then(response => {
-      console.log(response);
-      populateTinderHtml();
+      console.log("respo: ", response);
+      populateNinderHtml();
+      var prob = Math.random();
     })
     .catch(error => {
       console.log(error); // Print the error if one occurred
-      // populateTinderHtml();  // arriscado
+      populateNinderHtml();  // arriscado
     });
   
 };
 
-populateTinderHtml();
-populateTinderHtml();
-populateTinderHtml();
-populateTinderHtml();
-// chromeless.getHtml("https://boards.4chan.org/mu/")
-//   .then(html => console.log(html))
-//   .catch(error => console.log(error));
+var getSignupPages = function(instances = 1, startNumber = 0) {
+  var requestOptions = {
+    url: 'https://tinder.niqq.co/new',
+    json: true,
+    followAllRedirects: true, 
+    followOriginalHttpMethod: true,
+    headers: {
+      "Content-type": "application/json"
+    }
+  };
+
+  console.log("Starting from result #"+startNumber);
+  chromeless.googleQuery("https://www.google.com.br/search?q=site:*/cadastro&num=100&start="+startNumber, 2)
+    .then(results => {
+      console.log("Results: ", results);
+      // requestOptions.body = {urls: results};
+      // request.post(requestOptions);
+      // setTimeout(() => getSignupPages(instances, startNumber+(100*instances)), Math.random()*10000);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+var createInstances = function(number) {
+  var startNumber = 100;
+  for(let i = 0; i < number; i++){
+    getSignupPages(number, startNumber);
+    startNumber += 100;
+  }
+};
+
+// // getSignupPages(500, 0);
+// populateNinderScreenshots();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
+populateNinderHtml();
