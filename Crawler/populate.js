80c910e40a961d3a841c17b7bb1ed709835f3e1a
Bruno Miyamoto <bmiyamoto@gmail.com>
2017-10-17 18:46:04 -0200

    Crawler: populate com nova função initialize do downloader, corrigido tratamento da resolução ou rejeição do Promise.all e adicionado timeout geral para nunca travar

--- Crawler/populate.js
+++ Crawler/populate.js
@@ -3,8 +3,14 @@
 const Downloader = require('./downloader.js');
 const NinderClient = require('./ninderClient.js');
 
-class Populate{
-    constructor(){
+var timeoutPromise = function(durationMs) {
+    return new Promise((resolve, reject) => {
+        setTimeout(_ => reject(Error("ERR_POPULATE_TIMEOUT")), durationMs);      
+    });
+};
+
+class Populate {
+    constructor() {
         let self = this;
         self.client = new NinderClient();
         self.downloader = new Downloader();
@@ -20,27 +26,32 @@ class Populate{
     /*
         Popula o servidor com dados passíveis de classificação manual.
     */
-    populateDataset(){
-        let self = this;
-        let url = "";
-        return new Promise((resolve, reject) => {
-            self.client.getNextUrlToDownloadHtml()
+    populateDataset() {
+        const self = this;
+        let populatePromise = new Promise((resolve, reject) => {
+            console.log("\nPopulate: getting next url");
+            let url = "";
+            self.downloader.initialize()
+                .then(_ => self.client.getNextUrlToDownloadHtml())
                 .then(response => {
                     url = response.url;
                     return self.downloader.getDataFrom(self.tratar(url));
                 })
-                .then(data => {
-                    Promise.all(
-                        self.client.sendScreenshot(url, data[0]),
-                        self.client.sendHtml(url, data[1])
-                    );
+                .then(data => Promise.all([
+                        self.client.sendScreenshot(url, data.screenshot),
+                        self.client.sendHtml(url, data.html)
+                    ]))
+                .then(_ => {
+                    console.log("Populate: completed with success URL", url);
+                    resolve(true);
                 })
-                .then(_ => resolve(true))
                 .catch(error => {
+                    console.log("Populate: failed with error");
                     reject(error);
                 });
-
         });
+        
+        return Promise.race([populatePromise, timeoutPromise(30000)]);
     }
 }
 
